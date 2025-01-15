040e342ba719b90ac5b3bcf037af6af09fb7ea1a
U-glitsh <itsmosscow@gmail.com>
2025-01-15 04:33:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '201210307869'
+global.nomerowner = ["201210307869"]
 
 // Apikey 
 
